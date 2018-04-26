cd35c18d7729b8bffdff15a4f84b0898d3f5878f
Boredy <36232400+Boredy@users.noreply.github.com>
2018-04-26 19:33:56 -0400

    boganella

---
+++ FlamingoJavaFX/src/main/java/app/controller/BlackJackController.java
@@ -0,0 +1,91 @@
+package app.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import app.Flamingo;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import pkgCore.Action;
+import pkgCore.GamePlay;
+import pkgCore.Player;
+import pkgCore.Table;
+import pkgEnum.eAction;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.control.*;
+
+public class BlackJackController implements Initializable {
+
+	private Flamingo FlamingoGame;
+	@FXML private Label lblTopName;
+	@FXML private Label lblBottomName;
+	@FXML private Button btnTop;
+	@FXML private Button btnBottom;
+	
+
+	@FXML
+	private BorderPane mainScreen;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+	}
+
+	public void setMainApp(Flamingo FlamingoGame) {
+		this.FlamingoGame = FlamingoGame;
+	}
+
+	@FXML
+	public void btnSitLeave_Click(ActionEvent event) {
+		Button btn = (Button) event.getSource();
+
+		Action act = new Action();
+
+		act.setAction(btn.getText().equals("Sit")  ? eAction.Sit : eAction.Leave);
+		Player p = FlamingoGame.getAppPlayer();
+		if (btn.getId().equals("btnTop")) {
+			p.setiPlayerPosition(2);
+
+		} else if (btn.getId().equals("btnBottom")) {
+			p.setiPlayerPosition(0);
+		}
+
+		act.setPlayer(FlamingoGame.getAppPlayer());
+		FlamingoGame.SendMessageToClient(act);
+
+	}
+
+	public void HandleTableState(Table t) {
+		
+		Button btn = null;
+		Label lbl = null;
+		for (Player p: t.GetTablePlayers())
+		{
+			switch (p.getiPlayerPosition())
+			{
+			case 0:
+				btn = btnBottom;
+				lbl = lblBottomName;
+				btn.setText("Leave");
+				btnTop.setVisible(!p.getPlayerID().equals(FlamingoGame.getAppPlayer().getPlayerID()));
+
+				break;
+			case 2:
+				btn = btnTop;
+				lbl = lblTopName;
+				btn.setText("Leave");
+				btnBottom.setVisible(!p.getPlayerID().equals(FlamingoGame.getAppPlayer().getPlayerID()));
+				break;
+			}
+			btn.setVisible(p.getPlayerID().equals(FlamingoGame.getAppPlayer().getPlayerID()) );
+			lbl.setText(p.getPlayerName());
+		}
+
+	}
+
+	public void HandleGameState(GamePlay gp) {
+
+		// Coming Soon....!
+	}
+
+}
